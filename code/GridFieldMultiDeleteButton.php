c1b18ea5edfa33c2e0b2db9abe78709554a368d6
helpfulrobot <chris@silverstripe.com>
2015-12-18 09:14:50 +1300

    Converted to PSR-2

--- code/GridFieldMultiDeleteButton.php
+++ code/GridFieldMultiDeleteButton.php
@@ -9,28 +9,30 @@
  */
 class GridFieldMultiDeleteButton extends GridFieldApplyToMultipleRows
 {
-	/**
-	 * Shortcut to create a button that deletes all selected entries
-	 */
-	public function  __construct($targetFragment = 'after') {
-		parent::__construct('deleteselected', _t('GridFieldMultiDeleteButton.ButtonText','Delete Selected'), array($this, 'deleteRecord'), $targetFragment, array(
-			'icon'    => 'delete',
-			'class'   => 'deleteSelected',
-			'confirm' => _t('GridFieldMultiDeleteButton.Confirm', 'Are you sure you want to delete all selected items?'),
-		));
-	}
+    /**
+     * Shortcut to create a button that deletes all selected entries
+     */
+    public function __construct($targetFragment = 'after')
+    {
+        parent::__construct('deleteselected', _t('GridFieldMultiDeleteButton.ButtonText', 'Delete Selected'), array($this, 'deleteRecord'), $targetFragment, array(
+            'icon'    => 'delete',
+            'class'   => 'deleteSelected',
+            'confirm' => _t('GridFieldMultiDeleteButton.Confirm', 'Are you sure you want to delete all selected items?'),
+        ));
+    }
 
 
-	/**
-	 * @param DataObject $record
-	 * @param int $index
-	 */
-	public function deleteRecord($record, $index) {
-		if ($record->hasExtension('Versioned')) {
-			$record->deleteFromStage('Stage');
-			$record->deleteFromStage('Live');
-		} else {
-			$record->delete();
-		}
-	}
+    /**
+     * @param DataObject $record
+     * @param int $index
+     */
+    public function deleteRecord($record, $index)
+    {
+        if ($record->hasExtension('Versioned')) {
+            $record->deleteFromStage('Stage');
+            $record->deleteFromStage('Live');
+        } else {
+            $record->delete();
+        }
+    }
 }
